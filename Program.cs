838f6f94192dbb55419cf98d0345149b4d87b683
Hacı Muhammed <qwehacim3542qwe@gmail.com>
2024-01-14 21:07:10 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Kaç Adet Sayı Girilecek: ");
+int l = Convert.ToInt32(Console.ReadLine());
+
+List<int> notlar = new List<int>();
+for (int i = 1; i <= l; i++)
+{
+    Console.Write($"{i}. Sayıyı Giriniz: ");
+    int sayi = Convert.ToInt32(Console.ReadLine());
+    notlar.Add(sayi);
+}
+int sum = notlar.Sum();
+int avg = notlar.Sum() / notlar.Count;
+Console.WriteLine($"Toplam: {sum} Ortalama: {avg}");
