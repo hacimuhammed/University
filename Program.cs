6c9cef260ac8785b56b2397165b566df2b7a2d81
Hacı Muhammed <qwehacim3542qwe@gmail.com>
2024-01-14 21:00:38 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿double[] burslar = { 150, 600, 700, 450, 850, 275, 950, 1500, 210, 368 };
+
+int _i = 0;
+foreach (double burs in burslar)
+{
+    if (burs <= 500)
+        burslar[_i] += 0.1 + 50;
+    Console.WriteLine(burslar[_i++]);
+
+}
