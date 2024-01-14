c38b3b71dd76e804e27625a9b701a7c45960efbe
Hacı Muhammed <qwehacim3542qwe@gmail.com>
2024-01-14 21:04:31 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿string[] iller = { "Muş", "Ağrı", "Bolu", "Mersin", "Ankara", "Sivas", "Kayseri" };
+string[] isimler = { "Asuman", "İclal", "Emin", "Bekir", "Ahmet", "Can", "Esma" };
+
+Random random = new Random();
+random.Shuffle(isimler);
+
+int length = iller.Length;
+
+for (int i = 0; i < length - 1; i++)
+{
+    int j = random.Next(i, length);
+
+    if (j != i)
+    {
+        var temp = iller[i];
+        iller[i] = iller[j];
+        iller[j] = temp;
+    }
+}
+
+for (int i = 0; i < 7; i++)
+    Console.WriteLine(iller[i] + " " + isimler[i]);
