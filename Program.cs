bb85f5b773a2ba08b9db158c6d12acf2891e2d2c
Hacı <79199863+hacimuhammed@users.noreply.github.com>
2024-01-15 00:01:36 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,23 +1,9 @@
-﻿string[] iller = { "Muş", "Ağrı", "Bolu", "Mersin", "Ankara", "Sivas", "Kayseri" };
+string[] iller = { "Muş", "Ağrı", "Bolu", "Mersin", "Ankara", "Sivas", "Kayseri" };
 string[] isimler = { "Asuman", "İclal", "Emin", "Bekir", "Ahmet", "Can", "Esma" };
 
 Random random = new Random();
 random.Shuffle(iller);
 random.Shuffle(isimler);
-
-int length = iller.Length;
-
-for (int i = 0; i < length - 1; i++)
-{
-    int j = random.Next(i, length);
-
-    if (j != i)
-    {
-        var temp = iller[i];
-        iller[i] = iller[j];
-        iller[j] = temp;
-    }
-}
 
 for (int i = 0; i < 7; i++)
     Console.WriteLine(iller[i] + " " + isimler[i]);
